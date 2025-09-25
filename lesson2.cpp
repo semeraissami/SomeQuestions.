4de4e14473a791346802fc0fca18bd39fcb8bc0d
semeraissami <133181237+semeraissami@users.noreply.github.com>
2025-09-25 19:49:11 -0400

    Add files via upload
    Here are the code for lab 2

---
+++ lesson2.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    string age;
+    string program;
+    
+    
+    cout << " tell me your name ";
+    cin >> name;
+    cout << "hey nice to meet you Mr "  << name << endl;
+    
+    cout << "what is your age?" << endl;
+    cin >> age;
+    cout << "cool! " << endl;
+
+    cout <<"what is your program?" << endl;
+    cin >> program; 
+    
+    cout << "oh "<< program << endl;
+    cout << "hehe good luck with everything so you can make me more powerful! >:) muahaha ";
+
+
+    return 0;
+}
